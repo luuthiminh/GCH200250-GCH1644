bfebca064fc54f06265c8e2b35e709a885dde70a
luuthiminh <87143580+luuthiminh@users.noreply.github.com>
2022-06-01 09:29:48 +0700

    update
    main

--- expressdemo/main.js
+++ expressdemo/main.js
@@ -10,8 +10,12 @@ app.get('/', function(req, res){
 })
 
 app.get('/student', function(req, res){
-
-    res.render('student')
+    let foods = ['Com ', 'Bo ', 'Ga ', 'Mi tom']
+    let food2 = []
+    for(i = 0; i <foods.length; i++){
+        food2.push(foods[i].toUpperCase())
+    }
+    res.render('student', {'foods': food2})
 })
 
 app.listen(5000)
