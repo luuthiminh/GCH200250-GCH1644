1713d9620f0426f3991d67731ea4be74933253c3
luuthiminh <87143580+luuthiminh@users.noreply.github.com>
2022-06-15 08:37:12 +0700

    Update
    connect mongodb

--- mongodemo/main.js
+++ mongodemo/main.js
@@ -2,12 +2,15 @@ var express = require('express')
 const { listen } = require('express/lib/application')
 const res = require('express/lib/response')
 var app = express()
+var MongoClient = require('mongodb').MongoClient
+var url = "http://127.0.0.1/27017"
 
 app.get("/insert",(req, res)=>{
     res.render("newProduct")
 })
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({extended:true}))
+
 
 app.post('/newProduct', async(req, res)=>{
     let name = req.body.txtName
@@ -18,7 +21,15 @@ app.post('/newProduct', async(req, res)=>{
            'price' : price,
            'picture' : picture
     }
+    //Kết nối đến server có địa chỉ url
+    let server = await MongoClient.connect(url)
+    //Truy cập đến Databay ATMToys
+    let dbo = server.db("ATMToys")
     //insert product vao database
+    //Đợi hoàn thành bằng sử dụng await
+    await dbo.collections("product").insert(product)
+    //Quay lại trang home
+    res.redirect("/")
 })
 
 app.get('/',(req, res)=>{
