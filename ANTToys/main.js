abc26af73d04f85570905a67ce0d8307eef6e5e3
luuthiminh <87143580+luuthiminh@users.noreply.github.com>
2022-06-26 05:09:09 +0700

    Update
    category

--- ANTToys/main.js
+++ ANTToys/main.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var session = require('express-session')
 
+const ObjectId = require('mongodb').ObjectId;
+
 const { MongoClient } = require('mongodb')
 
 var mongoClient = require('mongodb').MongoClient
@@ -12,39 +14,17 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({extended:true}))
 
-// app.get('/viewAllToys',async (req,res)=>{
-//     //1. ket noi den server co dia chi trong url
-//     let server = await MongoClient.connect(url)
-//     //truy cap Database ATNToys
-//     let dbo = server.db("ATNToys")
-//     //get data
-//     let products = await dbo.collection('Toy').find().toArray()
-//     res.render('view',{'toys':toys})
-// })
-
-app.post('/search',async (req,res)=>{
-    let name = req.body.txtName
-
-    //1. ket noi den server co dia chi trong url
-    let server = await MongoClient.connect(url)
-    //truy cap Database ATNToys
-    let dbo = server.db("ATNToys")
-    //get data
-    let products = await dbo.collection('Toy').find({'name': new RegExp(name,'i')}).toArray()
-    res.render('allProduct',{'toys':toys})
-})
-
 app.get('/', async(req,res)=>{
-    // let name = req.body.txtName
-    // let price = req.body.txtPrice
-    // let description = req.body.txtDescription
-    // let picture = req.body.txtPicture
-    // let toys = {
-    //     'name': name,
-    //     'price': price,
-    //     'description' : description,
-    //     'picture': picture
-    // }
+    let name = req.body.txtName
+    let price = req.body.txtPrice
+    let description = req.body.txtDescription
+    let picture = req.body.txtPicture
+    let toys = {
+        'name': name,
+        'price': price,
+        'description' : description,
+        'picture': picture
+    }
        //1. ket noi den server co dia chi trong url
        let server = await MongoClient.connect(url)
        //truy cap Database ATNToys
@@ -53,6 +33,38 @@ app.get('/', async(req,res)=>{
        let toy = await dbo.collection("Toy").find().toArray()
        //quay lai trang home
     res.render('home',{'toy':toy})
+})
+app.post('/search',async (req,res)=>{
+    let name = req.body.txtName
+    //1. ket noi den server co dia chi trong url
+    let server = await MongoClient.connect(url)
+    //truy cap Database ATNToys
+    let dbo = server.db("ATNToys")
+    //get data
+    let toy = await dbo.collection("Toy").find({'name': new RegExp(name,'i')}).toArray()
+    res.render('home',{'toy':toy})
+})
+
+app.get('/detail/:_id',async (req,res)=>{
+    var id = req.params._id;
+    var good_id = new ObjectId(id);
+
+    //1. ket noi den server co dia chi trong url
+    let server = await MongoClient.connect(url)
+    //truy cap Database ATNToys
+    let dbo = server.db("ATNToys")
+    //get data
+    let toys = await dbo.collection('Toy').find({ '_id': good_id}).toArray()
+    res.render('detail',{'toys':toys})
+})
+
+app.get('/logout',(req,res)=>{
+    req.session.userName = null
+    req.session.save((err)=>{
+        req.session.regenerate((err2)=>{
+            res.redirect('/')
+        })
+    })
 })
 
 app.post('/create', async (req,res)=>{
@@ -80,6 +92,55 @@ app.post('/create', async (req,res)=>{
 app.get('/insert',(req,res)=>{
     res.render("create")
 })
+
+
+app.get('/update/:_id', async (req, res) => {
+    //transform your param into an ObjectId
+    let id = req.params._id;
+    let good_id = new ObjectId(id);
+    //1. kết nối đến server có địa chỉ trong url
+    let server = await MongoClient.connect(url) // await là đợi công việc này hoàn thành mới làm công việc tiếp theo. 
+    //phải có async mới dùng được await 
+    //2. truy cập database ATNToys
+    let dbo = server.db("ATNToys")
+    let toys = await dbo.collection('Toy').find({ '_id': good_id}).limit(1).toArray()
+    res.render('edit', {'toys': toys[0]})
+})
+
+app.post('/edit/:_id', async (req, res) => {
+    let name = req.body.txtName
+    let price = req.body.txtPrice
+    let description = req.body.txtDescription
+    let picture = req.body.txtPicture
+
+    //transform your param into an ObjectId
+    var id = req.params._id;
+    var good_id = new ObjectId(id);
+
+    //1. kết nối đến server có địa chỉ trong url
+    let server = await MongoClient.connect(url) // await là đợi công việc này hoàn thành mới làm công việc tiếp theo. 
+    //phải có async mới dùng được await 
+    //2. truy cập database ATNToys
+    let dbo = server.db("ATNToys")
+    let toys = await dbo.collection('Toy').updateOne({ '_id': good_id }, {$set:{'_id': good_id,'name': txtName, 'price': txtPrice, 'description': txtDescription, 'picture': txtPicture}})
+    res.redirect('/')
+})
+
+app.get('/delete/:_id', async (req, res) => {
+    //transform your param into an ObjectId
+    var id = req.params._id;
+    var good_id = new ObjectId(id);
+
+    //1. kết nối đến server có địa chỉ trong url
+    let server = await MongoClient.connect(url) // await là đợi công việc này hoàn thành mới làm công việc tiếp theo. 
+    //phải có async mới dùng được await 
+    //2. truy cập database ATNToys
+    let dbo = server.db("ATNToys")
+    await dbo.collection('Toy').deleteOne({'_id': good_id})
+    res.redirect('/')
+})
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
