323a814f167d7cf8a51e1e4cfad920f40e8e245c
luuthiminh <87143580+luuthiminh@users.noreply.github.com>
2022-06-26 01:14:36 +0700

    update
    main

--- ANTToys/main.js
+++ ANTToys/main.js
@@ -4,7 +4,7 @@ var session = require('express-session')
 const { MongoClient } = require('mongodb')
 
 var mongoClient = require('mongodb').MongoClient
-var url = 'mongodb://127.0.0.1:27017'
+var url = 'mongodb+srv://luuthiminh2028:Minh1234@cluster0.77xxnw3.mongodb.net/test'
 
 var app = express()
 
@@ -12,32 +12,55 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({extended:true}))
 
-app.get('/',function(req,res){
-    let id = req.body.txtId
+// app.get('/viewAllToys',async (req,res)=>{
+//     //1. ket noi den server co dia chi trong url
+//     let server = await MongoClient.connect(url)
+//     //truy cap Database ATNToys
+//     let dbo = server.db("ATNToys")
+//     //get data
+//     let products = await dbo.collection('Toy').find().toArray()
+//     res.render('view',{'toys':toys})
+// })
+
+app.post('/search',async (req,res)=>{
     let name = req.body.txtName
-    let price = req.body.txtPrice
-    let description = req.body.txtDescription
-    let categoryName = req.body.txtCategoryName
-    let picture = req.body.txtPicture
-    let toy = {
-        'id' : id,
-        'name': name,
-        'price': price,
-        'description' : description,
-        'categoryName' : categoryName,
-        'picture': picture
-    }
-    res.render('home',{'id':id, 'name':name, 'price':price, 'description':description, 'categoryName': categoryName, 'picture':picture})
+
+    //1. ket noi den server co dia chi trong url
+    let server = await MongoClient.connect(url)
+    //truy cap Database ATNToys
+    let dbo = server.db("ATNToys")
+    //get data
+    let products = await dbo.collection('Toy').find({'name': new RegExp(name,'i')}).toArray()
+    res.render('allProduct',{'toys':toys})
 })
 
-app.post('/createToy', async (req,res)=>{
-    let id = req.body.txtId
+app.get('/', async(req,res)=>{
+    // let name = req.body.txtName
+    // let price = req.body.txtPrice
+    // let description = req.body.txtDescription
+    // let picture = req.body.txtPicture
+    // let toys = {
+    //     'name': name,
+    //     'price': price,
+    //     'description' : description,
+    //     'picture': picture
+    // }
+       //1. ket noi den server co dia chi trong url
+       let server = await MongoClient.connect(url)
+       //truy cap Database ATNToys
+       let dbo = server.db("ATNToys")
+       //insert Toy
+       let toy = await dbo.collection("Toy").find().toArray()
+       //quay lai trang home
+    res.render('home',{'toy':toy})
+})
+
+app.post('/create', async (req,res)=>{
     let name = req.body.txtName
     let price = req.body.txtPrice
     let description = req.body.txtDescription
     let picture = req.body.txtPicture
-    let admin = {
-        'id' : id,
+    let toys = {
         'name': name,
         'price': price,
         'description': description,
@@ -47,10 +70,10 @@ app.post('/createToy', async (req,res)=>{
     let server = await MongoClient.connect(url)
     //truy cap Database ATNToys
     let dbo = server.db("ATNToys")
-    //insert product
-    await dbo.collection("Toy").insertOne(Toy)
+    //insert Toy
+    await dbo.collection("Toy").insertOne(toys)
     //quay lai trang home
-    res.render('create',{'id': id, 'name' : name, 'price':price, 'description':description,'picture': picture})
+    res.redirect('/')
 })
 
 //Gọi hàm lên gg, bên nav
